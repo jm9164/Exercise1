de8da7fa0aee9565ced7db959105156f51311872
Julian Mueller <julian.b.mueller@tum.de>
2017-11-13 15:02:37 +0100

    minor corrections

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,10 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("Hello ASE2017 - How are you now? :)");
-=======
-		System.out.println("Hello ASE2017 - How are you? :)");
 		System.out.println("What is your name?");
 		Scanner scanner = new Scanner(System.in);
 		String name = scanner.nextLine();
@@ -16,7 +13,6 @@ public class HelloWorld {
 		if (scanner != null) {
 			scanner.close();
 		}
->>>>>>> refs/heads/HelloUser
-	}
 
+}
 }
